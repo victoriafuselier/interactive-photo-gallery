5ff2b5b06f408f6b99fbb66c5e0e56541dad8aa9
Victoria Fuselier <vic.fuselier@gmail.com>
2023-05-02 16:25:46 -0500

    Completed JS search function
    After collaborating with Sof and seeing where I'd gone wrong before

--- js/app2.js
+++ js/app2.js
@@ -2,17 +2,16 @@ document.getElementById("search-bar").addEventListener("keyup", myFunction);
 
 function myFunction() {
     let searchBar = document.getElementById("search-bar");
-    searchInput = searchBar.value.toLowerCase();
+    let searchInput = searchBar.value.toLowerCase();
     let gallery = document.getElementById("gallery");
     let galleryPhotos = document.getElementsByClassName("gallery-photo");
-
-    for (i=0; i<searchInput.length; i++) {
-       let captionFilter = galleryPhotos[i].getAttribute('data-caption').toLowerCase();
-       console.log(captionFilter);
-       if (captionFilter.includes(searchInput)) {
-        galleryPhotos[i].style.display = '';
-       } else {
-        galleryPhotos[i].style.display = 'none';
-       }
+    
+    for (i=0; i<galleryPhotos.length; i++) {
+        let captionFilter = galleryPhotos[i].getAttribute('data-caption').toLowerCase();
+        if (captionFilter.includes(searchInput)) {
+            galleryPhotos[i].style.display = '';
+        } else {
+            galleryPhotos[i].style.display = 'none';
+        }
     }
 }
