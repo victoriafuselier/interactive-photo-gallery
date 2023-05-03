c1c9a0a01c11a84850d32e6eae99830d5b52e5a9
Victoria Fuselier <vic.fuselier@gmail.com>
2023-05-03 13:04:48 -0500

    Implemented changes for best practice on for loop
    Sof suggested this edit for best practice :)

--- js/app2.js
+++ js/app2.js
@@ -4,7 +4,7 @@ function myFunction() {
     let searchInput = document.getElementById('search-bar').value.toLowerCase();
     let galleryPhotos = document.getElementsByClassName("gallery-photo");
     
-    for (i=0; i<galleryPhotos.length; i++) {
+    for (let i=0; i < galleryPhotos.length; i++) {
         let captionFilter = galleryPhotos[i].getAttribute('data-caption').toLowerCase();
         if (captionFilter.includes(searchInput)) {
             galleryPhotos[i].style.display = '';
