7ffbd85b11507821188cf283271a234aab678e44
Victoria Fuselier <vic.fuselier@gmail.com>
2023-05-03 15:22:40 -0500

    Set variables with const instead of let
    Per sof's suggestion. Also changed the spacing in the loop i = 0

--- js/app2.js
+++ js/app2.js
@@ -1,10 +1,10 @@
 document.getElementById("search-bar").addEventListener("keyup", myFunction);
 
 function myFunction() {
-    let searchInput = document.getElementById('search-bar').value.toLowerCase();
-    let galleryPhotos = document.getElementsByClassName("gallery-photo");
+    const searchInput = document.getElementById('search-bar').value.toLowerCase();
+    const galleryPhotos = document.getElementsByClassName("gallery-photo");
     
-    for (let i=0; i < galleryPhotos.length; i++) {
+    for (let i = 0; i < galleryPhotos.length; i++) {
         let captionFilter = galleryPhotos[i].getAttribute('data-caption').toLowerCase();
         if (captionFilter.includes(searchInput)) {
             galleryPhotos[i].style.display = '';
